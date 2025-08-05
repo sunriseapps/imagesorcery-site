eed3781fa5af47d4b15f2ceec4624e84d2a555d0
titulus <titulus.web@gmail.com>
2025-08-05 11:33:53 +0300

    Refactor: move GA to index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Google Analytics GA4
-const GA_MEASUREMENT_ID = 'G-LGS2TRQZLZ';
-
-// Load gtag script
-const script1 = document.createElement('script');
-script1.async = true;
-script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
-document.head.appendChild(script1);
-
-// Initialize gtag
-const script2 = document.createElement('script');
-script2.innerHTML = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', '${GA_MEASUREMENT_ID}');
-`;
-document.head.appendChild(script2);
 
 createRoot(document.getElementById("root")!).render(<App />);
